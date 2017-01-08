3e2503ebe6a282d0ba1d81b24fd2565109f486ae
Seven <seven@megalive.com.tw>
2017-01-08 14:46:17 +0800

    Added example

---
+++ index.js
@@ -0,0 +1,25 @@
+const lazyMan = require('./libs');
+
+/**
+ *  Wait 3 second before introduction myself and eat.
+ */
+
+lazyMan('Ben').eat('breakfast').eat('dinner').sleepFirst(3);
+
+/**
+ * Wait 3 second before eat dinner but after introduction myself.
+ */
+
+// lazyMan('Ben').sleep(3).eat('dinner');
+
+/**
+ * Wait 3 second before eat dinner but after introduction myself and eat breakfast.
+ */
+
+// lazyMan('Ben').eat('breakfast').sleep('3').eat('dinner');
+
+ /**
+  * Wait 3 second after eat dinner and introduction myself.
+  */
+
+// lazyMan('Ben').eat('dinner').sleep(3);
